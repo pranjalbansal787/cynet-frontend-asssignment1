068232676d139d678e672888caca37c142857d4c
Pranjal Bansal <pranban12@gmail.com>
2024-06-23 23:44:14 +0530

    updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,8 +8,8 @@ export default defineConfig({
   build: {
     // Ensure that Rollup options are configured correctly
     rollupOptions: {
-      // Make sure not to externalize 'react' and 'react-dom' as they are needed for your app
-      external: ['lodash', 'chart.js', 'framer-motion', 'react-chartjs-2', 'react-gauge-chart', 'react-redux', 'redux'], // Add other libraries you do not want to bundle here
+      // Externalize dependencies that should not be bundled
+      external: ['redux', 'react-redux'], // Add other libraries you do not want to bundle here
     },
   },
 });
