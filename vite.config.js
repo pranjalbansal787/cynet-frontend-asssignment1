25e7883709287daba18a11c74158417c65acb7a2
Pranjal Bansal <pranban12@gmail.com>
2024-06-23 23:29:35 +0530

    Fixed vite.config.js file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(), // Use @vitejs/plugin-react for React support
+  ],
+  build: {
+    // Ensure that Rollup options are configured correctly
+    rollupOptions: {
+      // Make sure not to externalize 'react' and 'react-dom' as they are needed for your app
+      external: ['lodash', 'chart.js', 'framer-motion', 'react-chartjs-2', 'react-gauge-chart', 'react-redux', 'redux'], // Add other libraries you do not want to bundle here
+    },
+  },
+});
